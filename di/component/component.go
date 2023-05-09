feb7c370528dcf97c8153db538e7adbea3ac27d2
sabahtalateh <sabahtalateh@gmail.com>
2023-05-09 23:05:44 +0300

    wip

--- di/component/component.go
+++ di/component/component.go
@@ -12,6 +12,14 @@ func RegisterE[T any](_ func() *T) *Component[T] {
 	return &Component[T]{}
 }
 
+func Provider[T any](_ func() *T) *Component[T] {
+	return &Component[T]{}
+}
+
+func ProviderE[T any](_ func() *T) *Component[T] {
+	return &Component[T]{}
+}
+
 func (c *Component[T]) Name(name string) *Component[T] {
 	return c
 }
