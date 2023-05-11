da8f11ad2c09c37cddde46eda00736f0e1f48955
sabahtalateh <sabahtalateh@gmail.com>
2023-05-11 20:55:41 +0300

    wip

--- di/component/component.go
+++ di/component/component.go
@@ -4,14 +4,14 @@ type DiComponent[T any] struct {
 	t *T
 }
 
-func Component[T any](_ func() *T) *DiComponent[T] {
+func Component[T any](_ any) *DiComponent[T] {
 	return &DiComponent[T]{}
 }
 
-func Provider[T any](_ func() *T) *DiComponent[T] {
+func Provider[T any](_ any) *DiComponent[T] {
 	return &DiComponent[T]{}
 }
 
-func (c *DiComponent[T]) Name(name string) *DiComponent[T] {
+func (c *DiComponent[T]) Name(_ string) *DiComponent[T] {
 	return c
 }
