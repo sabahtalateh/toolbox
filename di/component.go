dc418dab52638c2f43e2ef1b3dbe1da58117055f
sabahtalateh <sabahtalateh@gmail.com>
2023-06-13 21:13:47 +0700

    wip

--- di/component.go
+++ di/component.go
@@ -3,12 +3,14 @@ package di
 // Component
 //
 // Parameter is a function returning structure or interface
-func Component(_ any) {
+// Following parameters are "value-functions"
+func Component(_ any, _ ...any) {
 }
 
 // NamedComponent
 //
 // First parameter is a component name
 // Second parameter is a function returning structure or interface
-func NamedComponent(_ string, _ any) {
+// Following parameters are "value-functions"
+func NamedComponent(_ string, _ any, _ ...any) {
 }
