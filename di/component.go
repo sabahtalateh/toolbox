4978c2c409a5ffd0958cc2a396304211e5e983d1
sabahtalateh <sabahtalateh@gmail.com>
2023-06-13 21:29:57 +0700

    wip

--- di/component.go
+++ di/component.go
@@ -7,7 +7,7 @@ type сomponent struct {
 //
 // Parameter is a function returning structure or interface
 // Following parameters are "value-functions"
-func Component(_ any, _ ...any) *сomponent {
+func Component(_ any) *сomponent {
 	return nil
 }
 
@@ -16,7 +16,7 @@ func Component(_ any, _ ...any) *сomponent {
 // First parameter is a component name
 // Second parameter is a function returning structure or interface
 // Following parameters are "value-functions"
-func NamedComponent(_ string, _ any, _ ...any) *сomponent {
+func NamedComponent(_ string, _ any) *сomponent {
 	return nil
 }
 
