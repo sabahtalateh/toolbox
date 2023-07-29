fc888b39ace4fdeff3fdc05cdd48dede7b35cc55
sabahtalateh <sabahtalateh@gmail.com>
2023-07-29 20:18:23 +0700

    wip

---
+++ component.go
@@ -0,0 +1,26 @@
+package toolbox
+
+type С struct {
+}
+
+// Component
+//
+// Parameter is a function returning structure or interface
+func Component(_ any) *С {
+	return nil
+}
+
+// Name
+//
+// Specify component name
+func (d *С) Name(_ string) *С {
+	return nil
+}
+
+// With
+//
+// First parameter is a component constructor function parameter name
+// Following parameters are functions returning components, structures or interfaces
+func (d *С) With(_ string, _ any, _ ...any) *С {
+	return nil
+}
